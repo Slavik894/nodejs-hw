ca9217d03f470d58da7628ea38bd3d743c391b3f
Slavik <slavik1058@gmail.com>
2026-09-12 21:32:43 +0200

    feat: implement notes CRUD operations and error handling with http-errors

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,6 +1,13 @@
+import { HttpError } from "http-errors";
+
 export const errorHandler = (err, req, res, next)=>{
     console.error(err);
 
+    if(err instanceof HttpError){
+      return res.status(err.status).json({
+        message: err.message || err.name,
+      });
+    }
      const isProd = process.env.NODE_ENV === "production";
 
   res.status(500).json({
@@ -8,4 +15,4 @@ export const errorHandler = (err, req, res, next)=>{
       ? "Something went wrong. Please try again later."
       : err.message,
   });
-}
+};
--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,19 +1,14 @@
 import {Router} from 'express';
+import { createNote, deleteNote, getNotes, getnotesById } from '../constrollers/notesController';
 
 const router = Router();
 
-router.get('/notes', (req, res)=>{
-    res.status(200).json({
-        "message": "Retrieved all notes"
-    });
-});
+router.get('/notes', getNotes);
 
-router.get('/notes/:noteId',(req, res)=>{ 
-    const {noteId} = req.params;
-    res.status(200).json({
-        "message": `Retrieved note with ID: ${noteId}`
-    });
-});
+router.get('/notes/:noteId', getnotesById);
+router.post('/notes', createNote);
+router.delete('/notes/:notesId', deleteNote);
+
 
 
 
